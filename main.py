18cbcab3fa5ceda1a386e4f30b5de21289d15e3d
sufiyanhamza07 <sufyan.hamza07@gmail.com>
2024-03-30 21:41:20 +0500

    Data Ingestion file code updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
